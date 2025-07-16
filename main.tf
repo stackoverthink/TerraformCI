f455d2df5fab669f6a2a4bd97d65bf2c47e5a5e4
stackoverthink <tom+git@daugh.me>
2025-07-16 09:24:18 -0400

    add-infrastructure2

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = "1playgroundstggittest21342"
+  name                     = "1playtest21342"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
